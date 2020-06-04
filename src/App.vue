c73b31bb07806fa989b3af4b69f2ed90b6fb888f
Ana Paula Oliva <anapaulafernandez.oliva@gmail.com>
2020-06-04 17:48:27 -0500

    :construction: Work in progress: Imports OWM's API key
    Open Weather Map Api

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    hello   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return { 
+      api_key: 'fe9e301d8bd87d8a247c7c4d1f2f6e4e'
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
